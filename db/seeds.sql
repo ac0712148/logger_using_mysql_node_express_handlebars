b161dd8781f7c9721e5538e9f0df74be7f8260c6
Albert Cervantes <cervanalbert@gmail.com>
2020-04-16 22:39:26 -0700

    added 3 entries to table

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('In n Out Double-Double', true);
+
+INSERT INTO burgers (burger_name, devored)
+VALUES ('Five Guys Bacon Cheeseburger', true);
+
+INSERT INTO burgers (burger_name)
+VALUES ("BJ's Classic Burger");
