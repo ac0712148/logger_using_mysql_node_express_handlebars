e6525d54bff0cf9b1e9c298309955471205f52d3
Albert Cervantes <cervanalbert@gmail.com>
2020-04-17 01:43:49 -0700

    added query connection for selectAll, insertOne, updateOne

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,32 @@
 const connection = require("../config/connection.js");
 
 const orm = {
-    selectAll: {},
-    insertOne: {},
-    updateOne: {},
+    selectAll: (table, cb) => {
+        connection.query("SELECT * FROM ??;", [table], (err, data) => {
+            if (err) {
+                throw err;
+            }
+            cb(data);
+        });
+    },
+
+    insertOne: (table, col, val, cb) => {
+        connection.query("INSERT INTO ?? (??) VALUES (?);", [table, col, val], (err, data) => {
+            if (err) {
+                throw err;
+            }
+            cb(data);
+        });
+    },
+
+    updateOne: (table, newVal, condition, cb) => {
+        connection.query("UPDATE ?? SET ? WHERE ?;", [table, newVal, condition], (err, data) => {
+            if (err) {
+                throw err;
+            }
+            cb(data);
+        });
+    },
 };
 
 module.exports = orm;
