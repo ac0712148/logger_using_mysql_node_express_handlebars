8de042b16a705f09e45927c1c17f46c21f2795b5
Albert Cervantes <cervanalbert@gmail.com>
2020-04-17 21:57:19 -0700

    added spacing and finalized server

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const home_controllerRoutes = require("./controllers/burger_controller")
+const routes = require("./controllers/burger_controller")
+
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-app.use(express.static("public"));
-app.use(home_controllerRoutes);
+
+app.use(routes);
+
 app.listen(PORT, () => {
     console.log("Server listening on: http://localhost:" + PORT);
 });
