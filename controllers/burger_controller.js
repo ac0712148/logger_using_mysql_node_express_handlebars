ee1eb21b75caf0a58a306ad7bba32374e8991ae2
Albert Cervantes <cervanalbert@gmail.com>
2020-04-17 22:39:48 -0700

    removed insertOne data parameter

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
 });
 
 router.post("/api/cats", (req, res) => {
-    burger.insertOne({ burger_Name: req.body.name, devoured: req.body.devoured }, (result) => {
+    burger.insertOne({ burger_name: req.body.burger_name }, (result) => {
         res.json({ id: result.insertID });
     });
 });
