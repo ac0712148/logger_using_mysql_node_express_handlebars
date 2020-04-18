2992f447cde2cff9f57020d67075d43757847b14
Albert Cervantes <cervanalbert@gmail.com>
2020-04-17 22:40:53 -0700

    changed insert test values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO burgers (burger_name, devoured) 
 VALUES ('In n Out Double-Double', true);
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ('Five Guys Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name)
+VALUES ('Five Guys Bacon Cheeseburger');
 
 INSERT INTO burgers (burger_name)
 VALUES ("BJ's Classic Burger");
