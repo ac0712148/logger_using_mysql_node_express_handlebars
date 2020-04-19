7e6ddf2835f240490fe0366a1502c28983324c5c
Albert Cervantes <cervanalbert@gmail.com>
2020-04-18 19:27:33 -0700

    added code for button event

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,11 +1,23 @@
 $(() => {
-    $(".create-form").on("submit", (event) => {
+    console.log("In Burgers...")
+    $(".change-devour").on("click", function() {
+
+        const burgerId = $(this).data("id");
+        $.ajax(`/api/burgers/${burgerId}`, {
+            type: "PUT"
+        }).then(() => {
+            location.reload();
+        });
+
+    });
+
+    $("#newBurger").on("click", (event) => {
         event.preventDefault();
 
         const newBurger = {
-            burger_name: $("ba").val().trim()
+            burger_name: $("#newBurger").val().trim()
         };
-
+        console.log("New Burger: " + newBurger);
         $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger,
